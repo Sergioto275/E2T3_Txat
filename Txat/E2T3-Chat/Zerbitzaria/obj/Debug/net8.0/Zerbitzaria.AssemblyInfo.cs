cc36d95216940e39c10fb90193eaaeec2d3830d8
Cobas2288 <acobasv232@fpsanturtzilh.eus>
2025-02-12 12:01:30 +0100

    API

--- Txat/E2T3-Chat/Zerbitzaria/obj/Debug/net8.0/Zerbitzaria.AssemblyInfo.cs
+++ Txat/E2T3-Chat/Zerbitzaria/obj/Debug/net8.0/Zerbitzaria.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Zerbitzaria")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+792d029e563137dc03f958e351425ac470c8720b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1f5263515cd2992a3ee94fff6881db4322568a6f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Zerbitzaria")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Zerbitzaria")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
