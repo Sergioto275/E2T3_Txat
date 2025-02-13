2f6764a85412cd632d7df72777ae063ea7554405
Cobas2288 <acobasv232@fpsanturtzilh.eus>
2025-02-13 10:01:28 +0100

    99.90%

--- Txat/E2T3-Chat/Bezeroa/obj/Release/net8.0-windows/Bezeroa.AssemblyInfo.cs
+++ Txat/E2T3-Chat/Bezeroa/obj/Release/net8.0-windows/Bezeroa.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Bezeroa")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1f5263515cd2992a3ee94fff6881db4322568a6f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5c3ac23ff9214211e81f4cfcf448f9864ea2d024")]
 [assembly: System.Reflection.AssemblyProductAttribute("Bezeroa")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Bezeroa")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
